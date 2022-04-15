c36fc8ec378035ce2299333ff83aed2d2d2465d1
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-15 09:10:22 +0300

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './componet/navbar/navbar.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
+   
 
   ],
   imports: [
